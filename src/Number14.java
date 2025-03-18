80be1d3523f4521b6a618ecbe78e9fcf454ae913
leewoojin1234 <geonui542@gmail.com>
2025-03-18 11:13:09 +0900

    feat :: Add Number14 Number15

---
+++ src/Number14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Number14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = 0;
+        while (count < 5) {
+            count++;
+            if (number % 2 == 1) {
+                number++;
+            }
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
