8022ebb6c6c6640da4c9510dc0d13dc4f1e39615
leewoojin1234 <geonui542@gmail.com>
2025-03-18 11:45:17 +0900

    feat :: Add Number18

---
+++ src/Number18.java
@@ -0,0 +1,13 @@
+public class Number18 {
+    public static void main(String[] args) {
+        for(int i = 0; ; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            if (i > 10) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
