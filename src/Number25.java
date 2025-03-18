94e5e8fa919daf592976ef8e18b9f6d58ea1fd85
leewoojin1234 <geonui542@gmail.com>
2025-03-18 16:18:22 +0900

    feat :: Add Number25

---
+++ src/Number25.java
@@ -0,0 +1,17 @@
+public class Number25 {
+    public static void main(String[] args) {
+        char a='A';
+        int n=0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 3; j > i; j--) {
+                System.out.print(a+" ");
+                a++;
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(n+" ");
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
