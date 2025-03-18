33e4e505df3b4b7676feed9f6809d364155c44c8
leewoojin1234 <geonui542@gmail.com>
2025-03-18 11:26:00 +0900

    feat :: Add Number17

---
+++ src/Number17.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Number17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int sum = 0;
+        int cnt = 0;
+        while (cnt < number) {
+            cnt++;
+            sum += cnt;
+            System.out.print(sum+" ");
+        }
+    }
+}
