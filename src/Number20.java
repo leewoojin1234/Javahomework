da85b8513289375a8fe540de27771ab118a3e00f
leewoojin1234 <geonui542@gmail.com>
2025-03-18 15:44:43 +0900

    feat :: Add Number20

---
+++ src/Number20.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
