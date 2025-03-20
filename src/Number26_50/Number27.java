c9e10d8861f2e67eeed40702d7169dd35fa99766
leewoojin1234 <geonui542@gmail.com>
2025-03-20 11:15:08 +0900

    feat :: 2829

--- src/Number26_50/Number27.java
+++ src/Number26_50/Number27.java
@@ -1,14 +1,18 @@
 package Number26_50;
 
+import java.util.Scanner;
+
 public class Number27 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = 2; i >= 1; i--) {
+        for (int i = n-1; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
